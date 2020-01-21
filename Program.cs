c63fb84af92c00c1c8b39a195d914f7006c51c52
Strahinja Laktovic <strahinja.laktovic@gmail.com>
2020-01-21 19:00:45 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.Spark.Sql;
+using static Microsoft.Spark.Sql.Functions;
+using Column = Microsoft.Spark.Sql.Column;
+
+namespace SparkClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataPath = "data/traffic-data.csv";
+            var Spark = SparkSession
+                           .Builder()
+                           .GetOrCreate();
+
+            var dataFrame = Spark
+                .Read()
+                .Option("header", true)
+                .Csv(dataPath);
+
+            GetCountriesSortedByMostSevereWinter(dataFrame, 2017);
+            ShowWeatherData(dataFrame);
+        }
+
+        static void GetCountriesSortedByMostSevereWinter(DataFrame dataFrame, int year)
+        {
+            var winterStart = $"{year}-12-22 00:00:00";
+            var winterEnd = $"{year + 1}-3-20 00:00:00";
+
+            dataFrame
+               .Filter(
+                   Col("Source").EqualTo("W")
+                       .And(Col("Severity").EqualTo("Severe"))
+                       .And(Col("Type").EqualTo("Cold"))
+                       .And(Col("StartTime(UTC)").Gt(Lit(winterStart))
+                       .And(Col("EndTime(UTC)").Lt(Lit(winterEnd))
+               )))
+               .GroupBy("State")
+               .Agg(Count(dataFrame["State"]))
+               .Sort(-Count(dataFrame["State"]))
+               .Show(50);
+        }
+
+        static void ShowWeatherData(DataFrame dataFrame)
+        {
+            string city = "San Francisco";
+            string startDate = "2010-01-01 00:00:00";
+            string endDate = "2020-01-01 00:00:00";
+            
+            Func<Column, Column> convertSeverity = Udf<string, int>(severityString =>
+            {
+                switch (severityString)
+                {
+                    case "Light":
+                        return 1;
+                    case "Moderate":
+                        return 2;
+                    case "Heavy":
+                        return 3;
+                    case "Severe":
+                        return 4;
+                    default:
+                        return 0;
+                }
+            });
+
+            dataFrame.Filter(
+                    Col("Source").EqualTo("W")
+                    .And(Col("Severity").IsIn("Light", "Moderate", "Heavy", "Severe"))
+                    .And(Col("City") == city)
+                    .And(Col("StartTime(UTC)").Between(startDate, endDate))
+                    .And(Col("EndTime(UTC)").Between(startDate, endDate))
+                )
+                .GroupBy("Type")
+                .Agg(
+                    Min(convertSeverity(dataFrame["Severity"])),
+                    Max(convertSeverity(dataFrame["Severity"])),
+                    Avg(convertSeverity(dataFrame["Severity"])),
+                    Stddev(convertSeverity(dataFrame["Severity"])))
+                .Show();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# Big data course - Spark
+
+## Dataset
+Step 1 : [Download data](https://osu.app.box.com/v/lstw-traffic-weather-v2)
+
+Step 2 : Extract to data folder
+
+Step 3 : Rename the extracted file to `traffic-data`
+
+## Spark MacOS
+Step 1 : Install Homebrew
+    /usr/bin/ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”
+
+Step 2 : Install xcode-select
+    xcode-select –install
+
+Step 3 : Install Java 1.8 sdk
+    Installation guide at Oracle
+
+Step 4 : Install Spark
+    brew install apache-spark
+
+Spark version 2.4.4 with hadoop 2.7
+
+## Microsoft spark worker 
+Worker installed in `~/`
+Worker version 0.8.0
+
+## Run project command
+
+```
+export DOTNET_WORKER_DIR="/home/strahinja/Microsoft.Spark.Worker-0.8.0/"
+
+copy SparkClient.dll to /home/strahinja/Microsoft.Spark.Worker-0.8.0/
+
+spark-submit --class org.apache.spark.deploy.dotnet.DotnetRunner --master local bin/Debug/netcoreapp2.2/microsoft-spark-2.4.x-0.7.0.jar dotnet bin/Debug/netcoreapp2.2/SparkClient.dll
+```
